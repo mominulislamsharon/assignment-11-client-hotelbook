9a2331f7030b809d8e6793aaae441fddf564c84a
Mominul <islammominul3378@gmail.com>
2024-05-26 04:59:56 +0600

    added banner

---
+++ src/Pages/Banner/Banner.jsx
@@ -0,0 +1,127 @@
+
+
+const Banner = () => {
+    return (
+        <div>
+            <div className="carousel relative w-full h-[600px] ">
+  <div id="slide1" className="carousel-item relative w-full">
+    <img src="https://images.unsplash.com/photo-1542314831-068cd1dbfeeb?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="w-full rounded-xl" />
+    <div className="absolute rounded-xl flex items-center w-full h-full left-0  top-0 bg-gradient-to-tr from-[#151515] to-[rgba(21, 21, 21, 0)]">
+      <div className="text-white  space-y-7 text-center  w-full">
+        <h2 className="text-6xl  font-bold">Enjoy Your Vacation With Us</h2>
+        <p>Encourage immediate action by implying limited availability or special promotions. A headline like Book Now and Save 20% on Your Summer Vacation  can create a sense of urgency</p>
+       
+        <div className="input md:ml-96 input-bordered w-1/3 ml-28 md:flex  items-center">
+        <input type="text" className="grow mb-2   text-black" placeholder="Search" />
+        <button className="btn  btn-primary rounded-full w-28 mt-5 mb-5">Search</button>
+        </div>
+      </div>
+    </div>
+    <div className="absolute flex justify-between gap-3 transform  -translate-y-1/2 left-5 right-5 bottom-0">
+      <a href="#slide6" className="btn btn-circle">❮</a> 
+      <a href="#slide2" className="btn btn-circle">❯</a>
+    </div>
+  </div> 
+  <div id="slide2" className="carousel-item relative w-full">
+    <img src="https://images.unsplash.com/photo-1582268611958-ebfd161ef9cf?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="w-full rounded-xl" />
+    <div className="absolute rounded-xl flex items-center w-full h-full left-0  top-0 bg-gradient-to-tr from-[#151515] to-[rgba(21, 21, 21, 0)]">
+      <div className="text-white space-y-7 text-center w-full">
+        <h2 className="text-6xl font-bold">Enjoy Your Vacation With Us</h2>
+        <p>Encourage immediate action by implying limited availability or special promotions. A headline like Book Now and Save 20% on Your Summer Vacation  can create a sense of urgency</p>
+        <div className="input ml-96 input-bordered w-1/3 flex items-center">
+        <input type="text" className="grow text-black" placeholder="Search" />
+        <button className="btn  btn-primary rounded-full w-28 mt-5 mb-5">Search</button>
+        </div>
+      </div>
+    </div>
+    <div className="absolute flex justify-between gap-3 transform -translate-y-1/2 left-5 right-5 bottom-0">
+      <a href="#slide1" className="btn btn-circle">❮</a> 
+      <a href="#slide3" className="btn btn-circle">❯</a>
+    </div>
+  </div>
+  <div id="slide3" className="carousel-item relative w-full">
+    <img src="https://images.unsplash.com/photo-1582268611958-ebfd161ef9cf?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="w-full rounded-xl" />
+    <div className="absolute rounded-xl flex items-center w-full h-full left-0  top-0 bg-gradient-to-tr from-[#151515] to-[rgba(21, 21, 21, 0)]">
+      <div className="text-white space-y-7 text-center w-full">
+        <h2 className="text-6xl font-bold">Enjoy Your Vacation With Us</h2>
+        <p>Encourage immediate action by implying limited availability or special promotions. A headline like Book Now and Save 20% on Your Summer Vacation  can create a sense of urgency</p>
+        <div className="input ml-96 input-bordered w-1/3 flex items-center">
+        <input type="text" className="grow text-black" placeholder="Search" />
+        <button className="btn  btn-primary rounded-full w-28 mt-5 mb-5">Search</button>
+        </div>
+      </div>
+    </div>
+    <div className="absolute flex justify-between gap-3 transform -translate-y-1/2 left-5 right-5 bottom-0">
+      <a href="#slide2" className="btn btn-circle">❮</a> 
+      <a href="#slide4" className="btn btn-circle">❯</a>
+    </div>
+  </div>
+  <div id="slide4" className="carousel-item relative w-full">
+    <img src="https://images.unsplash.com/photo-1444201983204-c43cbd584d93?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="w-full rounded-xl" />
+    <div className="absolute rounded-xl flex items-center w-full h-full left-0  top-0 bg-gradient-to-tr from-[#151515] to-[rgba(21, 21, 21, 0)]">
+      <div className="text-white space-y-7 text-center w-full">
+        <h2 className="text-6xl font-bold">Enjoy Your Vacation With Us</h2>
+        <p>Encourage immediate action by implying limited availability or special promotions. A headline like Book Now and Save 20% on Your Summer Vacation  can create a sense of urgency</p>
+        <div className="input ml-96 input-bordered w-1/3 flex items-center">
+        <input type="text" className="grow text-black" placeholder="Search" />
+        <button className="btn  btn-primary rounded-full w-28 mt-5 mb-5">Search</button>
+        </div>
+      </div>
+    </div>
+    <div className="absolute flex justify-between gap-3 transform -translate-y-1/2 left-5 right-5 bottom-0">
+      <a href="#slide3" className="btn btn-circle">❮</a> 
+      <a href="#slide5" className="btn btn-circle">❯</a>
+    </div>
+  </div>
+  <div id="slide5" className="carousel-item relative w-full">
+    <img src="https://images.unsplash.com/photo-1512917774080-9991f1c4c750?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="w-full rounded-xl" />
+    <div className="absolute rounded-xl flex items-center w-full h-full left-0  top-0 bg-gradient-to-tr from-[#151515] to-[rgba(21, 21, 21, 0)]">
+      <div className="text-white space-y-7 text-center w-full">
+        <h2 className="text-6xl font-bold">Enjoy Your Vacation With Us</h2>
+        <p>Encourage immediate action by implying limited availability or special promotions. A headline like Book Now and Save 20% on Your Summer Vacation  can create a sense of urgency</p>
+        <div className="input ml-96 input-bordered w-1/3 flex items-center">
+        <input type="text" className="grow text-black" placeholder="Search" />
+        <button className="btn  btn-primary rounded-full w-28 mt-5 mb-5">Search</button>
+        </div>
+      </div>
+    </div>
+    <div className="absolute flex justify-between gap-3 transform -translate-y-1/2 left-5 right-5 bottom-0">
+      <a href="#slide4" className="btn btn-circle">❮</a> 
+      <a href="#slide6" className="btn btn-circle">❯</a>
+    </div>
+  </div>
+  <div id="slide6" className="carousel-item relative w-full">
+    <img src="https://images.unsplash.com/photo-1416331108676-a22ccb276e35?q=80&w=2067&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="w-full rounded-xl" />
+    <div className="absolute rounded-xl flex items-center w-full h-full left-0  top-0 bg-gradient-to-tr from-[#151515] to-[rgba(21, 21, 21, 0)]">
+      <div className="text-white space-y-7 text-center w-full">
+        <h2 className="text-6xl font-bold">Enjoy Your Vacation With Us</h2>
+        <p>Encourage immediate action by implying limited availability or special promotions. A headline like Book Now and Save 20% on Your Summer Vacation  can create a sense of urgency</p>
+        <div className="input ml-96 input-bordered w-1/3 flex items-center">
+        <input type="text" className="grow text-black" placeholder="Search" />
+        <button className="btn  btn-primary rounded-full w-28 mt-5 mb-5">Search</button>
+        </div>
+      </div>
+    </div>
+    <div className="absolute flex justify-between gap-3 transform -translate-y-1/2 left-5 right-5 bottom-0">
+      <a href="#slide5" className="btn btn-circle">❮</a> 
+      <a href="#slide1" className="btn btn-circle">❯</a>
+    </div>
+  </div>
+</div>
+<div className="mb-24 w-full items-center">
+    
+            <div className="bg-[#333333] absolute mt-[-70px] ml-[250px]  shadow-lg p-12 rounded-lg">
+                <form action="" >
+                    <input className=" p-4" type="date" name="date" id="" placeholder="Date Arrival" />
+                    <input className="ml-6 p-4" type="date" name="" id="" placeholder="Date Departure" />
+                    <input className="ml-6 p-4" type="number" name="adult" id="" placeholder="Adult" />
+                    <input className="ml-6 p-4" type="number" name="child" id="" placeholder="Child" />
+                    <input className="ml-6 bg-white rounded-xl font-bold p-3 w-24" type="submit" value="Book Now" />
+                </form>
+            </div>
+</div>
+        </div>
+    );
+};
+
+export default Banner;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,15 @@
+import Banner from "../Banner/Banner";
+
+
 
 
 const Home = () => {
     return (
         <div>
             <h1>HOme page</h1>
+            <div className="container mx-auto">
+            <Banner></Banner>
+            </div>
         </div>
     );
 };
